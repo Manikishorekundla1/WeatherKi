56b5161c3cbbcf747b3f675db5d5929accf76dd9
Manikishore <manikishorekundla9@gmail.com>
2023-11-09 23:00:12 +0530

    update1.1

---
+++ app/src/main/java/uk/ac/tees/w9613019/weatherki/BrodCastActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.w9613019.weatherki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrodCastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brod_cast)
+    }
+}
