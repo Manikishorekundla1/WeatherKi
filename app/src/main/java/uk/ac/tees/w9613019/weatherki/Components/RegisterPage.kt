1a64c8cc77357608e723895b9030e85dbacbbbc4
Manikishore <manikishorekundla9@gmail.com>
2023-12-05 18:01:19 +0530

    Login page

--- app/src/main/java/uk/ac/tees/w9613019/weatherki/Components/RegisterPage.kt
+++ app/src/main/java/uk/ac/tees/w9613019/weatherki/Components/RegisterPage.kt
@@ -1,13 +1,13 @@
 package uk.ac.tees.w9613019.weatherki.Components
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -39,7 +40,9 @@ fun RegisterPage() {
     var password by remember { mutableStateOf("") }
     val context = LocalContext.current
     val control=LocalSoftwareKeyboardController.current
-    Column(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+    Column(modifier = Modifier.fillMaxSize().padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =Arrangement.Center ) {
 
 
         OutlinedTextField(value = firstName,
@@ -85,7 +88,7 @@ fun RegisterPage() {
         Button(onClick = { Toast.makeText(context,"Registering",
             Toast.LENGTH_SHORT).show() },
             modifier = Modifier.fillMaxWidth().height(40.dp)) {
-            control?.hide()
+
             Text(text = "Register")
             
         }
